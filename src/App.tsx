df1dd536cb0d414aa14e42216d19af01da33b831
Sir-Thom <Sir-Thom1702@pm.me>
2024-07-15 16:19:25 -0400

    chore: Update @tauri-apps/plugin-sql to version 2.0.0-beta.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
@@ -6,8 +6,19 @@ import GameView from "./components/Gameview";
 import Question from "./components/QuestionBox/Question";
 import AnswerButton from "./components/AnswerBox/AnswerBtn";
 import CharacterCreationPage from "./Pages/CharacterCreation";
-
+import Database from '@tauri-apps/plugin-sql';
 function App() {
+const setDb = async () => {
+  const db = Database.load("sqlite:test.db");
+  await (await db).execute("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)");
+  await (await db).execute("INSERT INTO test (name) VALUES (?)", ["test"]);
+  const result = await (await db).execute("SELECT * FROM test");
+  console.log(result);
+}
+useEffect(() => {
+  setDb();
+}
+, []);
 
 
   return (
